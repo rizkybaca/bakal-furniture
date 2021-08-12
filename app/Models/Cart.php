e7c9a20677ca2edd183a39b220436bb6d0d8dfcc
ta <akun.rizky.baca@gmail.com>
2021-08-12 11:52:25 +0700

    model transactionitem, make hasone to product

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -14,11 +14,11 @@ class Cart extends Model
     'products_id'
   ];
 
-  public function products()
+  public function product()
   {
     return $this->hasOne(Product::class, 'id', 'products_id');
   }
-  public function users()
+  public function user()
   {
     return $this->belongsTo(User::class, 'users_id', 'id');
   }
--- app/Models/TransactionItem.php
+++ app/Models/TransactionItem.php
@@ -7,5 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransactionItem extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $fillable = [
+    'users_id',
+    'products_id',
+    'transactions_id'
+  ];
+
+  public function product()
+  {
+    return $this->hasOne(Product::class, 'id', 'products_id');
+  }
+  // public function user()
+  // {
+  //   return $this->belongsTo(User::class, 'users_id', 'id');
+  // }
+  // public function transaction()
+  // {
+  //   return $this->hasOne(Transactions::class, 'transactions_id', 'id');
+  // }
 }
