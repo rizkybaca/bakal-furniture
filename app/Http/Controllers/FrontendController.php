4f034a6dbb5ab9ef13cc1ea5ab81b4b29cc5fdde
ta <akun.rizky.baca@gmail.com>
2021-08-12 12:53:36 +0700

    cart page blading

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -15,4 +15,9 @@ class FrontendController extends Controller
   {
     return view('pages.frontend.details');
   }
+
+  public function cart(Request $request)
+  {
+    return view('pages.frontend.cart');
+  }
 }
