005b71da05b97ad351d54782248e5490dfc16d8f
ta <akun.rizky.baca@gmail.com>
2021-08-12 12:46:17 +0700

    details page blading

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    public function index(Request $request)
-    {
-        return view('pages.frontend.index');
-    }
+  public function index(Request $request)
+  {
+    return view('pages.frontend.index');
+  }
+
+  public function details(Request $request, $slug)
+  {
+    return view('pages.frontend.details');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontendController::class, 'index'])->name('index');
 
+Route::get('/details/{slug}', [FrontendController::class, 'details'])->name('details');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
