9906ac390be601ca319dbba60f2e5f19a89e3297
ta <akun.rizky.baca@gmail.com>
2021-08-20 14:04:03 +0700

    view product

--- resources/views/pages/dashboard/product/index.blade.php
+++ resources/views/pages/dashboard/product/index.blade.php
@@ -41,11 +41,11 @@
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="mb-10">
-        <a href="{{ route('dashboard.product.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">
-          + create product
+        <a href="{{ route('dashboard.product.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold inline-block py-2 px-4 rounded shadow-lg">
+          + Create Productttt
         </a>
       </div>
-      <div class="shadow overflow-hidden sm-rounded-md">
+      <div class="shadow overflow-hidden sm-rounded-md mt-10">
         <div class="px-4 py-5 bg-white sm:p-6">
           <table id="crudTable">
             <thead>
@@ -56,6 +56,7 @@
                 <th>Actions</th>
               </tr>
             </thead>
+            <tbody></tbody>
           </table>
         </div>
       </div>
